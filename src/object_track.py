7dcb3c8f5f960cab3445b3d79a6fa1c5a037b298
Daeyong Kim <youbinlove607@gmail.com>
2025-09-19 11:18:08 +0900

    .

--- src/object_track.py
+++ src/object_track.py
@@ -47,12 +47,33 @@ def update_color_value(x, color, is_min):
 
 def load_config(config_path):
     # TODO: LAB-cal.json 파일을 읽어와서 전역 변수에 설정하기
-    pass
+    global l_min, a_min, b_min, l_max, a_max, b_max
+
+    with open(config_path, "r") as f:
+        init_data = json.load(f)
+
+    l_min = init_data["l_min"]
+    l_max = init_data["l_max"]
+    a_min = init_data["a_min"]
+    a_max = init_data["a_max"]
+    b_min = init_data["b_min"]
+    b_max = init_data["b_max"]
 
 
 def save_config(config_path):
     # TODO: 현재 설정된 전역 변수를 LAB-cal.json 파일로 저장하기
-    pass
+    with open(config_path, "w") as f:
+
+        save_data = {
+            "l_min": l_min,
+            "l_max": l_max,
+            "a_min": a_min,
+            "a_max": a_max,
+            "b_min": b_min,
+            "b_max": b_max,
+        }
+
+        json.dump(save_data, f, indent=4)
 
 
 def update_trackbar_positions():
@@ -66,13 +87,22 @@ def update_trackbar_positions():
 
 def find_biggest_contour(mask):
     # TODO: mask 변수 값으로 부터 연결된 객체 중 가장 큰 객체 찾기
-    pass
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    try:
+        biggest_area_contour = max(contours, key=cv2.contourArea)
+    except:
+        return
+    return biggest_area_contour
 
 
 def draw_boundingbox(image, contour):
     # TODO: 가장 큰 객체에 대해 외접하는 바운딩 박스 그리기, cv2.boundingRect() 사용
     # TODO: Rect: (x y w h) 형태로 좌표 출력, cv2.putText() 사용
-    pass
+    if contour is None:
+        return
+    x, y, w, h = cv2.boundingRect(contour)
+    cv2.rectangle(image,(x, y),(x+w, y+h), (0,255,0), 2)
+    cv2.putText(image, f"x: {x}, y: {y}", (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
 
 
 if __name__ == "__main__":
@@ -124,6 +154,7 @@ if __name__ == "__main__":
 
         # 가장 큰 객체에 대해 외접하는 바운딩 박스 그리기, cv2.boundingRect() 사용
         draw_boundingbox(masked_img, biggest_contour)
+
 
         # Show filtered result
         combined = np.hstack((img, masked_img))
